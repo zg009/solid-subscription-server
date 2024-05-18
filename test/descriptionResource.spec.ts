df27874f274cb0190c09da3570477f0f257ab2d6
Zach <zag0003@auburn.edu>
2024-05-18 16:22:09 -0500

    setting up test

---
+++ test/descriptionResource.spec.ts
@@ -0,0 +1,20 @@
+import { afterAll, beforeAll, expect } from "@jest/globals"
+import { DescriptionResource } from '../src/types/descriptionResource';
+import { NotificationChannel } from "../src/types/notificationChannel";
+import { SubscriptionService } from "../src/types/subscriptionService";
+
+describe('DescriptionResource type', () => {
+    let dr: DescriptionResource;
+    let id: string;
+    let channels: NotificationChannel[]
+    let subscriptions: SubscriptionService[]
+    
+    beforeAll(() => {
+
+    })
+
+    afterAll(() => {
+
+    })
+
+})
