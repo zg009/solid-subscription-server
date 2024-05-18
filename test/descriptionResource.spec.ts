cd44248d47bd6ca5af57091883f57a36470fed16
Zach <zag0003@auburn.edu>
2024-05-18 16:55:53 -0500

    more test setup

--- test/descriptionResource.spec.ts
+++ test/descriptionResource.spec.ts
@@ -2,7 +2,10 @@ import { afterAll, beforeAll, expect } from "@jest/globals"
 import { DescriptionResource } from '../src/types/descriptionResource';
 import { NotificationChannel } from "../src/types/notificationChannel";
 import { SubscriptionService } from "../src/types/subscriptionService";
+import { ChannelType } from "../src/types/utils";
+import { mkdirSync, rmdirSync } from "node:fs";
 
+const TEMP_FOLDER = './resources/'
 describe('DescriptionResource type', () => {
     let dr: DescriptionResource;
     let id: string;
@@ -10,11 +13,27 @@ describe('DescriptionResource type', () => {
     let subscriptions: SubscriptionService[]
     
     beforeAll(() => {
+        const ncid = "http://localhost:6060/notification-channel"
+        const topic = "http://localhost:6060/topic/profile"
+        let channelType = ChannelType.WebSocketChannel2023
+        const nc = new NotificationChannel(ncid, channelType, topic)
+        channels.push(nc)
 
+        const ssid = "http://localhost:6060/subscription-service.ttl"
+        channelType = ChannelType.WebSocketChannel2023
+        const ss = new SubscriptionService(ssid, channelType)
+        subscriptions.push(ss)
+
+        
+        mkdirSync(TEMP_FOLDER)
     })
 
     afterAll(() => {
+        rmdirSync(TEMP_FOLDER)
+    })
 
+    it('creates a new descriptionResource doc', () => {
+        
     })
 
 })
